b84e3337999881be64b1e72dfa3d3b991fe0f9f8
Danny <barakadan421@gmail.com>
2023-10-29 17:16:45 +0200

    use variables

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const app = require('./app');
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
+const app = require('./app');
+
+// console.log(process.env);
+const port = process.env.PORT;
+// const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
