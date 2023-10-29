1b9d2e5d43f42c95c50e6d509708d7991964ed0d
Danny <barakadan421@gmail.com>
2023-10-29 14:52:18 +0200

    refactor app js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const sessionRouter = require('./routes/sessionRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -9,152 +11,10 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
-const learningSession = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/learningsession-simple.json`)
-);
+// 2) ROUTES
 
-// 2) ROUTE HANDLER
-const getAllSessions = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: learningSession.length,
-    data: {
-      learningSession,
-    },
-  });
-};
-
-const getSession = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-  const learningSessions = learningSession.find((el) => el.id === id);
-
-  if (!learningSessions) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      learningSessions,
-    },
-  });
-};
-
-const createSession = (req, res) => {
-  //console.log(req.body);
-  const newId = learningSession[learningSession.length - 1].id + 1;
-  const newLearningSession = Object.assign({ id: newId }, req.body);
-  learningSession.push(newLearningSession);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/learningsession-simple.json`,
-    JSON.stringify(learningSession),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          learningSession: newLearningSession,
-        },
-      });
-    }
-  );
-};
-
-const updateSession = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-  const learningSessions = learningSession.find((el) => el.id === id);
-
-  if (!learningSessions) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      learningSessions: '<Updated learningSessions here...>',
-    },
-  });
-};
-
-const deleteSession = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-  const learningSessions = learningSession.find((el) => el.id === id);
-
-  if (!learningSessions) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-// 3) ROUTES
-app.route('/api/v1/skillswap').get(getAllSessions).post(createSession);
-
-app
-  .route('/api/v1/skillswap/:id')
-  .get(getSession)
-  .patch(updateSession)
-  .delete(deleteSession);
-
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+app.use('/api/v1/sessions', sessionRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = 3000;
 
