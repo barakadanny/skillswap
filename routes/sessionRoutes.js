cbf44de651ae5962b7d273e05d9b31d5a36bb4cf
Danny <barakadan421@gmail.com>
2023-10-29 15:44:08 +0200

    use checkId middleware

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const sessionController = require('./../controllers/sessionController');
 
+router.param('id', sessionController.checkId);
+
 router
   .route('/')
   .get(sessionController.getAllSessions)
