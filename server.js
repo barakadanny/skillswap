30c1699f2ba76fbab4813a4d524efb26dd6286a6
Danny <barakadan421@gmail.com>
2023-10-31 00:43:02 +0200

    server updated

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful 🔗✅🔗✅🔗✅');
+  });
+
 const port = process.env.PORT;
 // const port = process.env.PORT || 3000;
 app.listen(port, () => {
