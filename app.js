00b952ac7828b073a839bb3a32a9d8fcdb4a60bc
Danny <barakadan421@gmail.com>
2023-11-20 10:04:43 +0200

    update app.js

--- app.js
+++ app.js
@@ -15,8 +15,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 // 2) ROUTES
-
 app.use('/api/v1/sessions', sessionRouter);
+// console.log('running');
 app.use('/api/v1/users', userRouter);
+
+// app.all('*', (req, res, next) => {
+//   res.status(404).json({
+//     status: 'fail',
+//     message: `Can't find ${req.originalUrl} on this server!`,
+//   });
+// });
 
 module.exports = app;
