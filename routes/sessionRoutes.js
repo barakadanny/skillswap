4dab4e9708d9cde3559e21ad6ac8e14a44e953e5
Danny <barakadan421@gmail.com>
2023-11-20 10:06:56 +0200

    sessionRoutes

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const sessionController = require('./../controllers/sessionController');
 
+router.route('/session-stats').get(sessionController.getSessionStats);
+router.route('/monthly-plan/:year').get(sessionController.getMonthlyPlan);
+
 router
   .route('/')
   .get(sessionController.getAllSessions)
