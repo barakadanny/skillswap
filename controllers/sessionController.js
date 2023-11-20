af93d3ed6940166a479706ce5d1779579a0a8f80
Danny <barakadan421@gmail.com>
2023-11-20 10:05:19 +0200

    session controller

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,9 +1,17 @@
 const Session = require('./../models/sessionModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.getAllSessions = async (req, res) => {
   try {
-    const sessions = await Session.find();
+    // EXECUTE QUERY
+    const features = new APIFeatures(Session.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const sessions = await features.query;
 
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: sessions.length,
@@ -85,6 +93,118 @@ exports.deleteSession = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
+    });
+  }
+};
+
+exports.getSessionStats = async (req, res) => {
+  try {
+    const stats = await Session.aggregate([
+      {
+        $match: { maxParticipant: { $gte: 1 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numSessions: { $sum: 1 },
+          maxParticipants: { $max: '$maxParticipant' },
+          avgParticipants: { $avg: '$maxParticipant' },
+          minParticipants: { $min: '$maxParticipant' },
+        },
+      },
+      {
+        $sort: { avgParticipants: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+//
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Session.aggregate([
+      {
+        $unwind: '$date',
+      },
+      {
+        $match: {
+          date: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$date' },
+          numberOfSessions: { $sum: 1 },
+          sessions: { $push: '$subject' },
+        },
+      },
+      {
+        $addFields: {
+          month: {
+            $switch: {
+              branches: [
+                { case: { $eq: ['$_id', 1] }, then: 'January' },
+                { case: { $eq: ['$_id', 2] }, then: 'February' },
+                { case: { $eq: ['$_id', 3] }, then: 'March' },
+                { case: { $eq: ['$_id', 4] }, then: 'April' },
+                { case: { $eq: ['$_id', 5] }, then: 'May' },
+                { case: { $eq: ['$_id', 6] }, then: 'June' },
+                { case: { $eq: ['$_id', 7] }, then: 'July' },
+                { case: { $eq: ['$_id', 8] }, then: 'August' },
+                { case: { $eq: ['$_id', 9] }, then: 'September' },
+                { case: { $eq: ['$_id', 10] }, then: 'October' },
+                { case: { $eq: ['$_id', 11] }, then: 'November' },
+                { case: { $eq: ['$_id', 12] }, then: 'December' },
+              ],
+              default: 'Unknown',
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0, // Exclude _id field from the output
+        },
+      },
+      // sort from January to December
+      {
+        $sort: {
+          month: 1,
+        },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: plan.length,
+      data: {
+        plan,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
     });
   }
 };
