9fc2249ba90acea2ec65a069933e4a17aefc2d4d
Danny <barakadan421@gmail.com>
2023-11-20 10:10:24 +0200

    update app.js

--- app.js
+++ app.js
@@ -16,14 +16,13 @@ app.use(express.json());
 
 // 2) ROUTES
 app.use('/api/v1/sessions', sessionRouter);
-// console.log('running');
 app.use('/api/v1/users', userRouter);
 
-// app.all('*', (req, res, next) => {
-//   res.status(404).json({
-//     status: 'fail',
-//     message: `Can't find ${req.originalUrl} on this server!`,
-//   });
-// });
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
