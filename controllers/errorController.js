41d39a00b47d8f1b74bb993b6a973bfae3daaa71
Danny <barakadan421@gmail.com>
2023-11-23 14:51:21 +0200

    Handle errors and send appropriate response

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -34,6 +34,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
     sendErrorProd(err, res);
   }
 };
