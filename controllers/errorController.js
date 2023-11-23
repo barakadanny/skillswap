eb693e448e425f9fea6bef627c3c8b74b873ddae
Danny <barakadan421@gmail.com>
2023-11-23 13:43:44 +0200

    errorController update

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,23 @@
+const AppError = require('./../utils/appError');
+
 module.exports = (err, req, res, next) => {
   const status = err.statusCode || 500;
   const message = err.message;
   const data = err.data;
-  res.status(status).json({
-    status: err.status,
-    message,
-    data,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    res.status(status).json({
+      status: err.status,
+      error: err,
+      message,
+      stack: err.stack,
+      data,
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    res.status(status).json({
+      status: err.status,
+      message,
+      data,
+    });
+  }
 };
