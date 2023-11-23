9abdbc9b35f5cbafaa891d2d584d4a517ed771ef
Danny <barakadan421@gmail.com>
2023-11-23 17:16:04 +0200

    userSchema

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirmation: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
