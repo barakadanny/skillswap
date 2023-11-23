66a118688cf3b2aa54275b705d4c99dd0d33c0e0
Danny <barakadan421@gmail.com>
2023-11-23 16:38:57 +0200

    Handle unhandledRejection error

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ mongoose
 
 const port = process.env.PORT;
 // const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`Server started on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
