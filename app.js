ead4a28acc367d3f99013c47c4d483feeb2cdd93
Danny <barakadan421@gmail.com>
2023-11-24 15:18:28 +0200

    update app js

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
 // 2) ROUTES
 app.use('/api/v1/sessions', sessionRouter);
 app.use('/api/v1/users', userRouter);
