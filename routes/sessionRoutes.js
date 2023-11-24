0821ad35a09153d8532e58ee8939d09a29e226ac
Danny <barakadan421@gmail.com>
2023-11-24 15:19:30 +0200

    protect getAllsessions route

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const sessionController = require('./../controllers/sessionController');
+const authController = require('./../controllers/authController');
 
 router.route('/session-stats').get(sessionController.getSessionStats);
 router.route('/monthly-plan/:year').get(sessionController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(sessionController.getAllSessions)
+  .get(authController.protect, sessionController.getAllSessions)
   .post(sessionController.createSession);
 
 router
