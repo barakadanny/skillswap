54b0fa3b8bb7b4bed1c1f2ef238958a5730254c2
Danny <barakadan421@gmail.com>
2023-11-24 15:19:08 +0200

    select: false for password

--- models/userModel.js
+++ models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     minlength: 8,
-    // select: false,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -48,6 +48,13 @@ userSchema.pre('save', async function (next) {
 
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
