7631ff4ca286f0d680f4fd9b3037fe37c7511449
Danny <barakadan421@gmail.com>
2023-11-27 12:10:19 +0200

    limit request to 1000

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +9,24 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARE
+// 1) GLOBAL MIDDLEWARE
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+/**
+ * TODO:
+ *
+ * - Increase limiter when the user has a premium account
+ * eg:
+ */
+// Limit requests from the same IP
+const limiter = rateLimit({
+  max: 1000, // 1000 requests from the same IP in 1 hour
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 
