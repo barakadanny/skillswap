6e42ff63675fac6fc8247bdb6a2c95fd80711f6a
Danny <barakadan421@gmail.com>
2023-12-03 23:10:57 +0200

    Add protection for sessionRoutes

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -4,17 +4,31 @@ const sessionController = require('./../controllers/sessionController');
 const authController = require('./../controllers/authController');
 
 router.route('/session-stats').get(sessionController.getSessionStats);
-router.route('/monthly-plan/:year').get(sessionController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin'),
+    sessionController.getMonthlyPlan
+  );
 
 router
   .route('/')
-  .get(authController.protect, sessionController.getAllSessions)
-  .post(sessionController.createSession);
+  .get(sessionController.getAllSessions)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    sessionController.createSession
+  );
 
 router
   .route('/:id')
   .get(sessionController.getSession)
-  .patch(sessionController.updateSession)
+  .patch(
+    authController.protect,
+    authController.restrictTo('user'),
+    sessionController.updateSession
+  )
   .delete(
     authController.protect,
     authController.restrictTo('user', 'admin'),
